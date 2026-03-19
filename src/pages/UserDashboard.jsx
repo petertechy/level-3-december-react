423fa5f2f56943472972affedab2a3fdc95096e0
Petertechy <petertechy01@gmail.com>
2026-03-19 11:33:58 +0100

    Add user dashboard page

---
+++ src/pages/UserDashboard.jsx
@@ -0,0 +1,38 @@
+import axios from 'axios'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const UserDashboard = () => {
+    useEffect(() => {
+        getDashboard()
+    }, [])
+    let navigate = useNavigate()
+
+    const getDashboard = () =>{
+        let url = "http://localhost:5001/user/getdashboard"
+        let token = localStorage.token
+        axios.get(url,{
+            headers: {
+                "Authorization":  `Bearer ${token}`,
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            }
+        })
+        .then((response)=>{
+            if(!response.data.status){
+                navigate("/signin")
+            }
+        })
+        .catch((error)=>{
+            console.log(error)
+        })
+    }
+    
+  return (
+    <div>
+        <h1>I am the dashboard</h1>
+    </div>
+  )
+}
+
+export default UserDashboard
