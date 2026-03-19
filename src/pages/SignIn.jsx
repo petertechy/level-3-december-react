c72e72618b125a45a9b02d983da4f4dfcb823800
Petertechy <petertechy01@gmail.com>
2026-03-19 11:33:56 +0100

    Update sign in page

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,9 +1,12 @@
 import axios from 'axios'
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const SignIn = () => {
     const [email, setemail] = useState("")
     const [password, setpassword] = useState("")
+    const [message, setmessage] = useState("")
+    let navigate = useNavigate()
     const handleSignIn = () =>{
         console.log({email, password})
         let url = "http://localhost:5001/user/signin"
@@ -12,17 +15,25 @@ const SignIn = () => {
         axios.post(url, credential)
         .then((response)=>{
             console.log(response)
+            if(response.data.status){
+                localStorage.token = response.data.token
+                navigate("/user-dashboard")
+            }else{
+                console.log("Invalid Credential")
+                setmessage(response.data.message)
+            }
         })
         .catch((error)=>{
             console.log(error)
         })
     }
   return (
-    <div>
-        <h1>Sign In</h1>
-        <input type="text" placeholder='email' onChange={(e)=>setemail(e.target.value)}/>
-        <input type="text" placeholder='password' onChange={(e)=>setpassword(e.target.value)}/>
-        <button onClick={handleSignIn}>Sign In</button>
+    <div className='mx-auto col-7 rounded shadow p-3 my-4'>
+        <h1 className='text-center text-success fs-4'>Sign In</h1>
+        <p className='text-danger'>{message}</p>
+        <input className='form-control mb-3' type="text" placeholder='email' onChange={(e)=>setemail(e.target.value)}/>
+        <input className='form-control mb-3' type="text" placeholder='password' onChange={(e)=>setpassword(e.target.value)}/>
+        <button className='btn btn-success w-100' onClick={handleSignIn}>Sign In</button>
     </div>
   )
 }
